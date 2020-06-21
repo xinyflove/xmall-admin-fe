7a86e3650de0b113937ae90ad52cd1fb0cab0e39
Peak Xin <xinyflove@sina.com>
2020-06-21 23:25:42 +0800

    evn 搭建

--- src/app.jsx
+++ src/app.jsx
@@ -2,15 +2,21 @@
  * @Author: Peak Xin 
  * @Date: 2020-06-20 22:50:54 
  * @Last Modified by: Peak Xin
- * @Last Modified time: 2020-06-20 23:22:45
+ * @Last Modified time: 2020-06-21 22:41:15
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
+import './index.scss';
 
 ReactDOM.render( 
-    <h1> Hello World </h1>,
+    <div>
+        <i className="fa fa-address-book"></i>
+        <h1> Hello World </h1>
+    </div>
+    ,
     document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
@@ -6,10 +7,13 @@ module.exports = {
     entry: './src/app.jsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js'
+        publicPath: '/dist/',
+        filename: 'js/app.js'
     },
     module: {
-        rules: [{
+        rules: [
+            // react syntax handel
+            {
                 test: /\.jsx$/,
                 exclude: /(node_modules)/,
                 use: {
@@ -19,6 +23,7 @@ module.exports = {
                     }
                 }
             },
+            // css file handel
             {
                 test: /\.css$/,
                 /*use: [
@@ -30,12 +35,54 @@ module.exports = {
                     use: "css-loader"
                 })
             },
+            // sass file handel
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            // image file handel
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        name: 'resource/[name].[ext]'
+                    },
+                }, ],
+            },
+            // font file handel
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        name: 'resource/[name].[ext]'
+                    },
+                }, ],
+            },
+
         ]
     },
     plugins: [
+        // 处理html文件
         new HtmlWebPackPlugin({
             template: './src/index.html'
         }),
-        new ExtractTextPlugin("styles.css")
-    ]
+        // 独立css文件
+        new ExtractTextPlugin("css/[name].css"),
+        // 提出公共模块
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'js/base.js'
+        })
+    ],
+    devServer: {
+        //contentBase: './dist',
+        port: 8086,
+    },
 };

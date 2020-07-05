c9f33aed0e192160c6c6770f24bf8ffed63ee988
Peak Xin <xinyflove@sina.com>
2020-07-05 23:47:20 +0800

    开始搭建通用组件

--- src/app.jsx
+++ src/app.jsx
@@ -2,73 +2,31 @@
  * @Author: Peak Xin 
  * @Date: 2020-06-20 22:50:54 
  * @Last Modified by: Peak Xin
- * @Last Modified time: 2020-07-05 00:24:28
+ * @Last Modified time: 2020-07-05 23:22:49
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import { HashRouter as Router, Route, Link } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Link, Switch } from 'react-router-dom';
 
-class A extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+// 页面
+import Home from 'page/Home/index.jsx';// page为webpack.config.js里的resolve.alias中的page
+
+class App extends React.Component {
     render(){
         return (
-            <div>
-                Compenent A
+            <Router>
                 <Switch>
-                    <Route exact path={`${this.props.match.path}`} render={(route)=>{
-                            return <div>当前组件是不带参数的A</div>;
-                    }}/>
-                    <Route path={`${this.props.match.path}/sub`} render={(route)=>{
-                            return <div>当前组件A的sub路径</div>;
-                    }}/>
-                    <Route path={`${this.props.match.path}/:id`} render={(route)=>{
-                            return <div>当前组件是带参数的A，参数是：{route.match.params.id}</div>;
-                    }}/>
+                    <Route exact path='/' component={Home} />
+                    <Redirect from='*' to='/' />
                 </Switch>
-            </div>
-        );
-    }
-}
-
-class B extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    render(){
-        return <div>Compenent B</div>
-    }
-}
-
-class Wrapper extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    render(){
-        return (
-            <div>
-                <Link to='/a'>组件A</Link>
-                <br/>
-                <Link to='/a/123'>带参数的组件A</Link>
-                <br/>
-                <Link to='/a/sub'>组件A的子路径</Link>
-                <br/>
-                <Link to='/b'>组件B</Link>
-                {this.props.children}
-            </div>
+            </Router>
         );
     }
 }
 
 ReactDOM.render(
-    <Router>
-        <Wrapper>
-            <Route path='/a' component={A}></Route>
-            <Route path='/b' component={B}></Route>
-        </Wrapper>
-    </Router>,
+    <App>
+    </App>,
     document.getElementById('app')
 );
---
+++ src/page/home/index.jsx
@@ -0,0 +1,19 @@
+/*
+ * @Author: Peak Xin 
+ * @Date: 2020-07-05 23:11:10 
+ * @Last Modified by: Peak Xin
+ * @Last Modified time: 2020-07-05 23:42:18
+ */
+
+import React from 'react';
+import './index.css';
+
+class Home extends React.Component {
+    render(){
+        return (
+            <div>Home</div>
+        );
+    }
+}
+
+export default Home;
